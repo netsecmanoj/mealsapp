c08feb08848fb2ed1376a550f351522d5326954f
netsecmanoj <netsecmanoj@gmail.com>
2026-01-23 23:21:41 +0530

    6th

--- web/dev-dist/sw.js
+++ web/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-38bb0eb2'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.987d5386aos"
+    "revision": "0.ahsh8eigjn4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
